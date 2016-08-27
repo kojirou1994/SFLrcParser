8e85f9b0f1aba02b2a5dc937b4d40a300bee2f6d
Kojirou <kojirouhtc@gmail.com>
2016-08-27 18:03:53 +0800

    update

--- Sources/SFLrcParser.swift
+++ Sources/SFLrcParser.swift
@@ -1,22 +1,40 @@
+//
+//  SFLrcParser.swift
+//  SFLrcParser
+//
+//  Created by 王宇 on 2016/8/27.
+//
+//
+
 import Foundation
 
 struct SFLrcParser {
     let lyric: String
     
     init(rawLyric: String) {
-        var lyric = rawLyric
-        while let range = lyric.range(of: "\n") {
-            lyric.removeSubrange(range)
-        }
-        self.lyric = lyric
+//        var lyric = rawLyric
+//        while let range = lyric.range(of: "\n") {
+//            lyric.removeSubrange(range)
+//        }
+        self.lyric = rawLyric
     }
     
     func parse() -> [SFLyric] {
-        let lines = lyric.components(separatedBy: "[").filter { $0.characters.count > 0 && $0.contains("]")}
-        var lyrics = lines.map { line -> SFLyric in
+        let lines = lyric.components(separatedBy: "\n").filter { $0.characters.count > 0 && $0.hasPrefix("[") && $0.contains("]")}
+        var lyrics = [SFLyric]()
+        lines.forEach({ line in
             let comp = line.components(separatedBy: "]")
-            return SFLyric(time: comp[0], text: comp.count > 1 ? comp[1] : "")
-        }
+            let time = comp[0]
+            let text = comp.count > 1 ? comp[1] : ""
+            if time.characters.count == 9 && time[time.index(time.startIndex, offsetBy: 3)] == ":" && time[time.index(time.startIndex, offsetBy: 6)] == "." {
+                do {
+                    let lyric = try SFLyric(time: time, text: text)
+                    lyrics.append(lyric)
+                } catch {
+                    return
+                }
+            }
+        })
         lyrics.sort{ $0 < $1 }
         return lyrics
     }
--- Sources/SFLyric.swift
+++ Sources/SFLyric.swift
@@ -8,14 +8,21 @@
 
 import Foundation
 
+enum SFLyricError: Error {
+    case timeSyntaxError
+}
+
 struct SFLyric {
     var time: Int
     var text: String
     
-    init(time: String, text: String) {
-        let minute = Int(time.substring(to: time.index(time.startIndex, offsetBy: 2))) ?? 0
-        let second = Int(time[time.index(time.startIndex, offsetBy: 3)..<time.index(time.startIndex, offsetBy: 5)]) ?? 0
-        let mileSecond = Int(time[time.index(time.startIndex, offsetBy: 6)..<time.index(time.startIndex, offsetBy: 8)]) ?? 0
+    init(time: String, text: String) throws {
+        let time = time.substring(from: time.index(time.startIndex, offsetBy: 1))
+        guard let minute = Int(time.substring(to: time.index(time.startIndex, offsetBy: 2))),
+            let second = Int(time[time.index(time.startIndex, offsetBy: 3)..<time.index(time.startIndex, offsetBy: 5)]),
+            let mileSecond = Int(time[time.index(time.startIndex, offsetBy: 6)..<time.index(time.startIndex, offsetBy: 8)]) else {
+            throw SFLyricError.timeSyntaxError
+        }
         self.time = (minute * 60 + second) * 1000 + mileSecond * 10
         self.text = text
     }
